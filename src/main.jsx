22be7cd0d2b81efa74003835af12b6a23d92a878
João Emanuel <jepleite13@gmail.com>
2026-06-21 05:00:46 -0300

    feat: finalização da refinação e primeira parte

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AlunoProvider } from "./context/AlunoContext";
 
-import "./index.css";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
